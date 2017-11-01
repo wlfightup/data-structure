420b68fd52ca822fcb68949148d58898d7d21af3
wangliang <wangliang@hualala.com>
2017-11-01 12:05:01 +0800

    get perm by optimal way

---
+++ perm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int get_prime(int);
+
+int main(int argc, char *argv[])
+{
+        get_prime(100);
+}
+
+int get_prime(int max_prime)
+{
+        unsigned long long c, l, n=0;
+        double t;
+        for(c=2; c<max_prime; c++)
+        {
+                t = sqrt((double)c);
+                for(l = 2; l <= t; l++)
+                        if (c % l == 0)
+                                break;
+                if (l > t )
+                {
+                        n++;
+                        printf("%d\n", c);
+                }
+        }
+}
